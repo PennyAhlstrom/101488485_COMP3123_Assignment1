c957deb541105f6ef7f6037e0108f91f3a8de100
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2025-10-11 16:07:31 -0400

    updated emp and user routes with validators

--- src/routes/emp.routes.js
+++ src/routes/emp.routes.js
@@ -8,19 +8,27 @@ const {
   deleteEmployeeById
 } = require('../controllers/emp.controller');
 
+const validate = require('../middleware/validate');
+const {
+  createEmployeeValidator,
+  updateEmployeeValidator,
+  getEmployeeByIdValidator,
+  deleteEmployeeValidator
+} = require('../validators/employee.validators');
+
 // GET all employees
 router.get('/employees', listEmployees);
 
 // Create employee
-router.post('/employees', createEmployee);
+router.post('/employees', createEmployeeValidator, validate, createEmployee);
 
 // Get specified employee (by id)
-router.get('/employees/:emp_id', getEmployeeById);
+router.get('/employees/:emp_id', getEmployeeByIdValidator, validate, getEmployeeById);
 
 // Update employee by id
-router.put('/employees/:emp_id', updateEmployeeById);
+router.put('/employees/:emp_id', updateEmployeeValidator, validate, updateEmployeeById);
 
 // Delete by query param -> 204
-router.delete('/employees', deleteEmployeeById);
+router.delete('/employees', deleteEmployeeValidator, validate, deleteEmployeeById);
 
 module.exports = router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/user.controller');
+const validate = require('../middleware/validate');
+const { signupValidator, loginValidator } = require('../validators/user.validators');
 
-
-router.post('/signup', signup); // 201 - created
-router.post('/login',  login);  // 200 - ok
+router.post('/signup',  signupValidator, validate, signup); // 201 - created
+router.post('/login',  loginValidator, validate, login);  // 200 - ok
 
 module.exports = router;
