2e34b8a0be254ac72d34ad7c4aad77d3bb9b1a85
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2025-10-11 15:24:57 -0400

    updated server.js

---
+++ src/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config(); // use information in .env file
+
+// core dependencies
+const express = require('express'); // Web framework, server core
+const cors = require('cors');       // Cross-origin resource sharing (front-end react can call from a different domain)
+const connectDB = require('./config/db'); // database connection
+
+// importning routes
+const userRoutes = require('./routes/user.routes');
+const empRoutes = require('./routes/emp.routes');
+
+const app = express(); // create the app (represents entire web server)
+
+// middleware
+app.use(cors()); // allows request from frontend (other origins)
+app.use(express.json()); // parses JSON to access req.body
+
+
+// routes
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/emp', empRoutes);
+
+
+// start server (after DB connects)
+const PORT = process.env.PORT || 8080; // PORT is specified in .env - default 8080
+connectDB(process.env.MONGODB_URI) // attemps connection to mondodb
+  .then(() => {
+    app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+  })
+  .catch((err) => {
+    console.error('Mongo connection failed:', err);
+    process.exit(1);
+  });
