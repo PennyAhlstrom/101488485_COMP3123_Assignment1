eb9f5beab9476ed3116fab7bff5891e204452661
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2025-10-11 16:01:28 -0400

    updated server.js with morgan and 404 Not Found

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ require('dotenv').config(); // use information in .env file
 const express = require('express'); // Web framework, server core
 const cors = require('cors');       // Cross-origin resource sharing (front-end react can call from a different domain)
 const connectDB = require('./config/db'); // database connection
+const morgan = require('morgan');    // HTTP request logger middleware  
 
 // importning routes
 const userRoutes = require('./routes/user.routes');
@@ -14,12 +15,17 @@ const app = express(); // create the app (represents entire web server)
 // middleware
 app.use(cors()); // allows request from frontend (other origins)
 app.use(express.json()); // parses JSON to access req.body
+app.use(morgan('dev')); // logs incoming request to console
 
 
 // routes
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/emp', empRoutes);
 
+// 404 fallback
+app.use((req, res) => {
+  res.status(404).json({ status: false, message: 'Not found' });
+});
 
 // start server (after DB connects)
 const PORT = process.env.PORT || 8080; // PORT is specified in .env - default 8080
