e749872c7cd64d02e578e53e648eddb51cff356c
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2025-11-30 14:50:15 -0500

    updated emp validators

--- src/validators/employee.validators.js
+++ src/validators/employee.validators.js
@@ -10,12 +10,26 @@ const createEmployeeValidator = [
   body('department').trim().notEmpty().withMessage('Enter the department')
 ];
 
+// Validation failed - changed to this to test
 const updateEmployeeValidator = [
   param('emp_id').isMongoId().withMessage('Employee ID must be a valid Mongo ID'),
+
+  body('first_name').optional().trim().notEmpty().withMessage('First name cannot be empty'),
+  body('last_name').optional().trim().notEmpty().withMessage('Last name cannot be empty'),
   body('email').optional().isEmail().withMessage('The email address must be valid'),
-  body('salary').optional().isNumeric().withMessage('The salary must be a number'),
-  body('date_of_joining').optional().isISO8601().toDate().withMessage('Enter a valid date')
+  body('position').optional().trim().notEmpty().withMessage('Position cannot be empty'),
+  body('salary').optional().isNumeric().withMessage('Salary must be a number'),
+  body('date_of_joining').optional().isISO8601().toDate().withMessage('Enter a valid date'),
+  body('department').optional().trim().notEmpty().withMessage('Department cannot be empty')
 ];
+
+
+// const updateEmployeeValidator = [
+//   param('emp_id').isMongoId().withMessage('Employee ID must be a valid Mongo ID'),
+//   body('email').optional().isEmail().withMessage('The email address must be valid'),
+//   body('salary').optional().isNumeric().withMessage('The salary must be a number'),
+//   body('date_of_joining').optional().isISO8601().toDate().withMessage('Enter a valid date')
+// ];
 
 const getEmployeeByIdValidator = [
   param('emp_id').isMongoId().withMessage('Employee ID must be a valid Mongo ID')
